fdd6c522ef559b48d688ecd1fafc663f48d12b85
unknown <kukos700@gmail.com>
2024-06-09 19:55:13 +0200

    settings append slash

--- delivering_service/delivering_service/settings.py
+++ delivering_service/delivering_service/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-le7+a=@f0srkltg0_-mj=0u=+z%fimls3*f4c&b_au)paa$w10
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
