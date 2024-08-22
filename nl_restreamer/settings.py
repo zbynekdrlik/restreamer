f4c923b8b1ad3ff3ac996aa1517edb4858ebd8c1
unknown <kukos700@gmail.com>
2024-08-22 22:17:16 +0200

    reset password feature

--- nl_restreamer/settings.py
+++ nl_restreamer/settings.py
@@ -214,6 +214,15 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 CSRF_TRUSTED_ORIGINS = ["https://restreamer.solarnyexprex.eu"]
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True  # Use TLS (Transport Layer Security)
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587  # Correct port for TLS
+EMAIL_HOST_USER = 'kusprogramus@gmail.com'  # Replace with your email
+EMAIL_HOST_PASSWORD = 'bhjvilpetdalmfvb'  # App password or regular password if 2FA is not enabled
+DEFAULT_FROM_EMAIL = 'kusprogramus@gmail.com'  # From email
+
+
 AWS_STORAGE_BUCKET_NAME = 'linode52'
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
