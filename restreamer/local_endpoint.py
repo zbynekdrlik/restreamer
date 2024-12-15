f0eacc4e1d40d0f1e4361cd9439d699b9e9648d7
Kristián Kampczyk <83923485+kuskryptus@users.noreply.github.com>
2024-12-15 22:25:45 +0100

    upading requirements.txt

--- restreamer/local_endpoint.py
+++ restreamer/local_endpoint.py
@@ -6,6 +6,8 @@ import time
 import redis
 import requests
 import boto3
+
+from django.conf import settings
 from restreamer.models import ChunkRecord
 
 importlib.invalidate_caches()
@@ -97,62 +99,58 @@ class ChunkSender:
         chunk_identifier = {"chunk_identifier": self.streaming_event_identifier}
         identifier = f"{chunk_id}_{chunk_identifier}.bin"
         chunk_size = {"chunk_size" : len(chunk_data)}
-        
-        uploaded = False
-        
+     
         while True:
             try:
                 self.upload_to_s3(chunk_data, identifier)
                 log.info(f"S3 upload for chunk {chunk_id} succeeded!")
-                uploaded = True
                 break
             except Exception as e:
                 log.warning(f"S3 upload failed for chunk {chunk_id}: {e}")
                 time.sleep(3)
                 continue
        
-        if uploaded:
-            retries = 0
-            while True:
-                if retries == 1:
-                    if self.check_chunk_server(chunk_id):
-                        log.info(f"Chunk{chunk_id} sent successfully ------------------ Chunk arrived ---------!")
-                        chunk.send = True
-                        chunk.save()
-                        return
-                retries += 1
-                self.streaming_event.refresh_from_db()
-                if not self.streaming_event.delivering_activated:
-                    log.info(f'Shutting down chunk send thread: {chunk_id}')
-                    return
-                try:
-                    data_payload = {**chunk_id, **chunk_identifier, **chunk_size}
-                    response = requests.post(
-                        self.api_url, data=data_payload, timeout=5
-                    )
-                except (requests.exceptions.Timeout, requests.exceptions.ConnectionError) as e:
-                    log.warning(f"Lost internet connection and {e}")
-                    log.info(f"Checking if reaceived {chunk_id} ...")
-                    if self.check_chunk_server(chunk_id):
-                        log.info(f"Chunk{chunk_id} sent successfully ------------------ Chunk arrived ---------!")
-                        chunk.send = True
-                        chunk.save()
-                        return
-                    time.sleep(3)
-                    continue
-
-                if response.status_code == 200:
-                    log.info(f"Chunk{chunk_id} sent successfully!")
-                    # chunk_record.delete()
+        retries = 0
+        while True:
+            if retries == 1:
+                if self.check_chunk_server(chunk_id):
+                    log.info(f"Chunk{chunk_id} sent successfully ------------------ Chunk arrived ---------!")
                     chunk.send = True
-                    chunk.delete()
-                    break
-                else:
-                    log.error(
-                        f"Failed to send chunk{chunk_id}. Status code: {response.status_code}"
-                    )
-                    log.info(f"Retrying Chunk{chunk_id} ...")
-                    time.sleep(1)
+                    chunk.save()
+                    return
+            retries += 1
+            self.streaming_event.refresh_from_db()
+            if not self.streaming_event.delivering_activated:
+                log.info(f'Shutting down chunk send thread: {chunk_id}')
+                return
+            try:
+                data_payload = {**chunk_id, **chunk_identifier, **chunk_size}
+                response = requests.post(
+                    self.api_url, data=data_payload, timeout=5
+                )
+            except (requests.exceptions.Timeout, requests.exceptions.ConnectionError) as e:
+                log.warning(f"Lost internet connection and {e}")
+                log.info(f"Checking if reaceived {chunk_id} ...")
+                if self.check_chunk_server(chunk_id):
+                    log.info(f"Chunk{chunk_id} sent successfully ------------------ Chunk arrived ---------!")
+                    chunk.send = True
+                    chunk.save()
+                    return
+                time.sleep(3)
+                continue
+
+            if response.status_code == 200:
+                log.info(f"Chunk{chunk_id} sent successfully!")
+                # chunk_record.delete()
+                chunk.send = True
+                chunk.delete()
+                break
+            else:
+                log.error(
+                    f"Failed to send chunk{chunk_id}. Status code: {response.status_code}"
+                )
+                log.info(f"Retrying Chunk{chunk_id} ...")
+                time.sleep(1)
 
     def check_chunk_server(self, chunk_id):
         chunk = ChunkRecord.objects.get(id=chunk_id['chunk_id'])
@@ -181,4 +179,5 @@ class ChunkSender:
                             Key=filename,)
         except Exception as e:
             log.exception(e)
+            raise
         
