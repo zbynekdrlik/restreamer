7470aeae36cd36718c3cae10f447de748377400f
Kristián Kampczyk <83923485+kuskryptus@users.noreply.github.com>
2024-12-15 21:20:45 +0100

    upading requirements.txt

--- nl_restreamer/settings.py
+++ nl_restreamer/settings.py
@@ -13,6 +13,7 @@ import os
 import sys
 from pathlib import Path
 from dotenv import load_dotenv
+import boto3
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 from decouple import config
