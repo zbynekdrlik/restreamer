dda7d9974b517e556a422d0b907c34f0fc65a2a1
Kristián Kampczyk <83923485+kuskryptus@users.noreply.github.com>
2024-12-26 17:33:27 +0100

    adding view for returning next chunk id for delivering service

--- restreamer/urls/urls.py
+++ restreamer/urls/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
 from restreamer.views.stream_management import ChunkUploadView, PositionLastUploadView, DeleteChunksView, ChunkExistsView
-from restreamer.views.communication import GetActiveStream, DeliveringReady
+from restreamer.views.communication import GetActiveStream, DeliveringReady, GetNextChunkView
 
 
 
 
 urlpatterns = [
     path("chunk-upload/", ChunkUploadView.as_view(), name="receive-local-chunks"),
+    
     path("api/update_position_last/", PositionLastUploadView.as_view(), name="update_position_last"),
     path("api/delete_all_chunks/", DeleteChunksView.as_view(), name="delete_all_chunks"),
     path('api/check-chunk/', ChunkExistsView.as_view(), name='check_chunk'),
     path('api/get_active_stream/', GetActiveStream.as_view(), name='get_active_stream'),
+    path("api/get-next-chunk/", GetNextChunkView.as_view(), name="get_next_chunk"),
+    
     path('check-status/<int:streaming_event_id>/', DeliveringReady.as_view(), name='stream_ready')
 ]
 
