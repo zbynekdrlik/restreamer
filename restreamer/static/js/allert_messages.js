e7088f0b561fce6618187e4e02f77c8368bc9453
Kristian Kampczyk <kukos700@gmail.com>
2025-02-11 19:56:10 +0100

    Update Bootstrap dependencies and clean up alert message scripts

--- restreamer/static/js/allert_messages.js
+++ restreamer/static/js/allert_messages.js
@@ -15,7 +15,6 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 });
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 function showBootstrapToast(message, type) {
     // Create (or get) a container for the toasts.
